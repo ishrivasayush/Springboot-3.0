1f19ff37c6b70b4c2b3c6b007a10428e1eac73ab
Ayush Shrivastava <ayusrivastava2k3@gmail.com>
2023-11-24 00:43:02 +0530

    Final

--- src/main/java/com/org/Springbootsecurity/config/SecurityConfig.java
+++ src/main/java/com/org/Springbootsecurity/config/SecurityConfig.java
@@ -3,24 +3,65 @@ package com.org.Springbootsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+// @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
             httpSecurity
                     .csrf().disable()
                     .authorizeHttpRequests()
+                    .requestMatchers("/home/admin")
+                    .hasRole("ADMIN")
+                    .requestMatchers("/home/normal")
+                    .hasRole("NORMAL")
                     .requestMatchers("/home/public")
                     .permitAll()
                     .and()
                     .formLogin();
 
             return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService()
+    {
+        UserDetails normalUser= User.withUsername("ayush")
+                .password(passwordEncoder().encode("ayush@123"))
+                .roles("NORMAL")
+                .build();
+        UserDetails adminUser= User.withUsername("abhay")
+                .password(passwordEncoder().encode("abhay@123"))
+                .roles("ADMIN")
+                .build();
+
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager(normalUser, adminUser);
+        return manager;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsServices()
+    {
+
+        return new CustomUserDetailService();
     }
 }
--- src/main/java/com/org/Springbootsecurity/controllers/HomeController.java
+++ src/main/java/com/org/Springbootsecurity/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.org.Springbootsecurity.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/home")
 public class HomeController {
+
+    // @PreAuthorize("hasRole('ADMIN')") method level security
+
     @GetMapping("/normal")
     public ResponseEntity<String> normalUser()
     {
